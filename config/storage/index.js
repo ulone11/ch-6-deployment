a93e8021780aeba8738a28a35344bff36498a94b
ulone11 <ulwan.style@gmail.com>
2024-08-23 19:59:37 +0700

    first push

--- config/storage/index.js
+++ config/storage/index.js
@@ -1,14 +1,5 @@
 const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/images");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
 const storage = multer.memoryStorage();
 
 const upload = multer({
