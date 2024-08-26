4e000c77189b0d6baa841b0bc1d3c941b761ed8c
ulone11 <ulwan.style@gmail.com>
2024-08-26 21:21:01 +0700

    feat: instrument and Sentry

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
+const Sentry = require("@sentry/node");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("./instrument");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+Sentry.setupExpressErrorHandler(app);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
