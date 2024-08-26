a69b9a229b65278a31d5fb673cf93996baf4f749
ulone11 <ulwan.style@gmail.com>
2024-08-26 21:31:59 +0700

    feat: Sentry error test

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 Sentry.setupExpressErrorHandler(app);
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/post", postRouter);
